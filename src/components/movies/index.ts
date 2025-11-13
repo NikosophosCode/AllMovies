a95b9b005df3134d092c121c0d481a3188f030cb
Nicolas Acuña <niconiampira@hotmail.com>
2025-11-12 20:46:15 -0500

    refactor(components): export new HeroSlider component from index
    - Add HeroSlider and HeroSlide exports to movies component index
    - Enable convenient imports from @/components/movies
    - Maintain consistency with component export pattern

--- src/components/movies/index.ts
+++ src/components/movies/index.ts
@@ -1,3 +1,4 @@
 export { default as MovieCard } from './MovieCard'
 export { default as MovieGrid } from './MovieGrid'
 export { default as MovieCarousel } from './MovieCarousel'
+export { HeroSlider, HeroSlide } from './HeroSlider'
